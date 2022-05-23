928772254f5e9c26e4d5620187ef29ab840f1270
Kasun De Silva <kasund@kasund.local>
2022-05-23 21:21:00 +1200

    Adding pickup summary

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 # Upload folder
-UPLOAD_FOLDER = 'static/files'
+UPLOAD_FOLDER = 'uploads/files'
 DB = 'db'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DB'] = DB
@@ -25,85 +25,49 @@ def index():
 @app.route('/order_list')
 def order_list():
     # Set The upload HTML template '\templates\order_list.html'
-    col_names = ['date', 'chicken', 'egg', 'vegetable', 'pickup_location', 'delivery_address', 'order_placed_by',
+    col_names = ['date', 'chicken', 'egg', 'vegetable', 'pickup_location', 'delivery_address', 'distribution_center', 'order_placed_by',
                  'contact_number', 'comments', 'tusker_name', 'amount']
-    csvData = pd.read_csv('db/order_list.csv', names=col_names, header=None, delimiter="\t")
+    csvData = pd.read_csv('db/order_list.csv',
+                          names=col_names, header=None, delimiter="\t")
     return render_template("order_list.html", data=csvData.to_html())
 
-@app.route('/chicken')
-def order_list():
+
+@app.route('/summary')
+def summary():
     # Set The upload HTML template '\templates\order_list.html'
-    col_names = ['date', 'chicken', 'egg', 'vegetable', 'pickup_location', 'delivery_address', 'order_placed_by',
+    col_names = ['date', 'chicken', 'egg', 'vegetable', 'pickup_location', 'distribution_center', 'delivery_address', 'order_placed_by',
                  'contact_number', 'comments', 'tusker_name', 'amount']
-    csvData = pd.read_csv('db/order_list.csv', names=col_names, header=None, delimiter="\t")
-    return render_template("order_list.html", data=csvData.to_html())
+    data = getOrderListSummary(pd.read_csv(
+        'db/order_list.csv', names=col_names, header=None, delimiter="\t"))
+    pickUpData = getPickUpSummary(pd.read_csv(
+        'db/order_list.csv', names=col_names, header=None, delimiter="\t"))
+    return render_template("summary.html", chicken=data[0], egg=data[1], vegetable=data[2], pickUpChickenNewLands=pickUpData[0][0],
+                           pickUpChickenChurtonPark=pickUpData[0][1], pickUpChickenLowerHutt=pickUpData[
+                               0][2], pickUpEggNewLands=pickUpData[1][0],
+                           pickUpEggChurtonPark=pickUpData[1][1], pickUpEggLowerHutt=pickUpData[
+                               1][2], pickUpVegeNewLands=pickUpData[2][0],
+                           pickUpVegeChurtonPark=pickUpData[2][1], pickUpVegeLowerHutt=pickUpData[2][2])
 
-# @app.route('/egg')
-# def order_list():
-#     # Set The upload HTML template '\templates\order_list.html'
-#     col_names = ['date', 'chicken', 'egg', 'vegetable', 'pickup_location', 'delivery_address', 'order_placed_by',
-#                  'contact_number', 'comments', 'tusker_name', 'amount']
-#     csvData = pd.read_csv('db/order_list.csv', names=col_names, header=None, delimiter="\t")
-#     return render_template("order_list.html", data=csvData.to_html())
 
-# @app.route('/vegetable')
-# def order_list():
-#     # Set The upload HTML template '\templates\order_list.html'
-#     col_names = ['date', 'chicken', 'egg', 'vegetable', 'pickup_location', 'delivery_address', 'order_placed_by',
-#                  'contact_number', 'comments', 'tusker_name', 'amount']
-#     csvData = pd.read_csv('db/order_list.csv', names=col_names, header=None, delimiter="\t")
-#     return render_template("order_list.html", data=csvData.to_html())
-
-# @app.route('/newlands')
-# def order_list():
-#     # Set The upload HTML template '\templates\order_list.html'
-#     col_names = ['date', 'chicken', 'egg', 'vegetable', 'pickup_location', 'delivery_address', 'order_placed_by',
-#                  'contact_number', 'comments', 'tusker_name', 'amount']
-#     csvData = pd.read_csv('db/order_list.csv', names=col_names, header=None, delimiter="\t")
-#     return render_template("order_list.html", data=csvData.to_html())
-
-# @app.route('/churtonpark')
-# def order_list():
-#     # Set The upload HTML template '\templates\order_list.html'
-#     col_names = ['date', 'chicken', 'egg', 'vegetable', 'pickup_location', 'delivery_address', 'order_placed_by',
-#                  'contact_number', 'comments', 'tusker_name', 'amount']
-#     csvData = pd.read_csv('db/order_list.csv', names=col_names, header=None, delimiter="\t")
-#     return render_template("order_list.html", data=csvData.to_html())
-
-# @app.route('/lowerhutt')
-# def order_list():
-#     # Set The upload HTML template '\templates\order_list.html'
-#     col_names = ['date', 'chicken', 'egg', 'vegetable', 'pickup_location', 'delivery_address', 'order_placed_by',
-#                  'contact_number', 'comments', 'tusker_name', 'amount']
-#     csvData = pd.read_csv('db/order_list.csv', names=col_names, header=None, delimiter="\t")
-#     return render_template("order_list.html", data=csvData.to_html())
-
-# @app.route('/city')
-# def order_list():
-#     # Set The upload HTML template '\templates\order_list.html'
-#     col_names = ['date', 'chicken', 'egg', 'vegetable', 'pickup_location', 'delivery_address', 'order_placed_by',
-#                  'contact_number', 'comments', 'tusker_name', 'amount']
-#     csvData = pd.read_csv('db/order_list.csv', names=col_names, header=None, delimiter="\t")
-#     return render_template("order_list.html", data=csvData.to_html())
-
-# Get the uploaded files
+# Upload the order list csv
 @app.route("/", methods=['POST'])
 def uploadFiles():
     # get the uploaded file
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
         # set the file path
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+        file_path = os.path.join(
+            app.config['UPLOAD_FOLDER'], uploaded_file.filename)
         # save the file
         uploaded_file.save(file_path)
         # Initiate parseCSV function to read the data
         parseCSV(file_path)
-        return redirect("order_list")
+        return redirect("summary")
 
 
 def parseCSV(filePath):
     # CVS Column Names
-    col_names = ['date', 'chicken', 'egg', 'vegetable', 'pickup_location', 'distribution_center', 'delivery_address', 'order_placed_by',
+    col_names = ['date', 'chicken', 'egg', 'vegetable', 'pickup_location', 'delivery_address', 'distribution_center', 'order_placed_by',
                  'contact_number', 'comments', 'tusker_name', 'amount']
     # Use Pandas to parse the CSV file
     csvData = pd.read_csv(filePath, names=col_names, header=None)
@@ -116,38 +80,187 @@ def generateOrderList(csvData):
     with open(os.path.join(app.config['DB'], "order_list.csv"), "w") as fo:
         for i, row in csvData.iterrows():
             if i > 0:
-                print(i, row['date'], row['chicken'], row['egg'], row['vegetable'], row['pickup_location'],
-                      row['delivery_address'], row['order_placed_by'], row['contact_number'], row['comments'],
-                      row['tusker_name'], row['amount'], )
-                fo.writelines(str(row['date']) + "\t" + str(row['chicken']) + "\t" + str(row['egg']) + ",\t" +
+
+                fo.writelines(str(row['date']) + "\t" + str(row['chicken']) + "\t" + str(row['egg']) + "\t" +
                               str(row['vegetable']) + "\t" + str(row['pickup_location']) + "\t" +
-                              str(row['delivery_address']) + str(row['distribution_center']) + "\t" +
+                              str(row['delivery_address']) + "\t" + str(row['distribution_center']) + "\t" +
                               str(row['order_placed_by']) + "\t" + str(row['contact_number']) + "\t" +
                               str(row['comments']) + "\t" + str(row['tusker_name']) + "\t" + str(row['amount']) + "\n")
 
 
-def generateChickenOrderList(csvData):
+def getOrderListSummary(csvData):
+    chickenOrders = 0
+    eggOrders = 0
+    vegeOrders = 0
+
     # Loop through the Rows
-    with open(os.path.join(app.config['DB'], "order_list.csv"), "w") as fo:
-        for i, row in csvData.iterrows():
-            if i > 0:
-                if int(row['chicken']) > 0:
-                    print(i, row['date'], row['chicken'], row['egg'], row['vegetable'], row['pickup_location'],
-                          row['delivery_address'], row['order_placed_by'], row['contact_number'], row['comments'],
-                          row['tusker_name'], row['amount'], )
-                    fo.writelines(str(row['date']) + "\t" + str(row['chicken']) + "\t" + str(row['egg']) + ",\t" +
-                                  str(row['vegetable']) + "\t" + str(row['pickup_location']) + "\t" +
-                                  str(row['delivery_address']) + "\t" + str(row['distribution_center']) + "\t" + str(row['order_placed_by']) + "\t" +
-                                  str(row['contact_number']) + "\t" + str(row['comments']) + "\t" +
-                                  str(row['tusker_name']) + "\t" + str(row['amount']) + "\n")
+    for i, row in csvData.iterrows():
+        if i > 0:
+            if int(row['chicken']) > 0:
+                chickenOrders = chickenOrders + int(row['chicken'])
+
+    for i, row in csvData.iterrows():
+        if i > 0:
+            if int(row['egg']) > 0:
+                eggOrders = eggOrders + int(row['egg'])
+
+    for i, row in csvData.iterrows():
+        if i > 0:
+            if int(row['vegetable']) > 0:
+                vegeOrders = vegeOrders + int(row['vegetable'])
+
+    return [chickenOrders, eggOrders, vegeOrders]
 
 
-# def generateEggOrderList(csvData):
-# def generateVegetableOrderList(csvData):
-# def generateNewlandsList(csvData):
-# def generateChurtonParkList(csvData):
-# def generateLowerHuttList(csvData):
-# def generateCityList(csvData):
+def getPickUpSummary(csvData):
+    pickUpChickenNewlandsOrders = 0
+    pickUpChickenChurtonParkOrders = 0
+    pickUpChickenLowerHuttOrders = 0
+    pickUpEggNewlandsOrders = 0
+    pickUpEggChurtonParkOrders = 0
+    pickUpEggLowerHuttOrders = 0
+    pickUpVegeNewlandsOrders = 0
+    pickUpVegeChurtonParkOrders = 0
+    pickUpVegeLowerHuttOrders = 0
+
+    # Loop through the Rows
+    for i, row in csvData.iterrows():
+        if i > 0:
+            if int(row['chicken']) > 0 and row['pickup_location'] == 'Newlands':
+                pickUpChickenNewlandsOrders = pickUpChickenNewlandsOrders + \
+                    int(row['chicken'])
+    for i, row in csvData.iterrows():
+        if i > 0:
+            if int(row['chicken']) > 0 and row['pickup_location'] == 'Churton Park':
+                pickUpChickenChurtonParkOrders = pickUpChickenChurtonParkOrders + \
+                    int(row['chicken'])
+    for i, row in csvData.iterrows():
+        if i > 0:
+            if int(row['chicken']) > 0 and row['pickup_location'] == 'Lower Hutt':
+                pickUpChickenLowerHuttOrders = pickUpChickenLowerHuttOrders + \
+                    int(row['chicken'])
+
+    for i, row in csvData.iterrows():
+        if i > 0:
+            if int(row['egg']) > 0 and row['pickup_location'] == 'Newlands':
+                pickUpEggNewlandsOrders = pickUpEggNewlandsOrders + \
+                    int(row['egg'])
+    for i, row in csvData.iterrows():
+        if i > 0:
+            if int(row['egg']) > 0 and row['pickup_location'] == 'Churton Park':
+                pickUpEggChurtonParkOrders = pickUpEggChurtonParkOrders + \
+                    int(row['egg'])
+    for i, row in csvData.iterrows():
+        if i > 0:
+            if int(row['egg']) > 0 and row['pickup_location'] == 'Lower Hutt':
+                pickUpEggLowerHuttOrders = pickUpEggLowerHuttOrders + \
+                    int(row['egg'])
+
+    for i, row in csvData.iterrows():
+        if i > 0:
+            if int(row['egg']) > 0 and row['pickup_location'] == 'Newlands':
+                pickUpVegeNewlandsOrders = pickUpVegeNewlandsOrders + \
+                    int(row['vegetable'])
+    for i, row in csvData.iterrows():
+        if i > 0:
+            if int(row['egg']) > 0 and row['pickup_location'] == 'Churton Park':
+                pickUpVegeChurtonParkOrders = pickUpVegeChurtonParkOrders + \
+                    int(row['vegetable'])
+    for i, row in csvData.iterrows():
+        if i > 0:
+            if int(row['egg']) > 0 and row['pickup_location'] == 'Lower Hutt':
+                pickUpVegeLowerHuttOrders = pickUpVegeLowerHuttOrders + \
+                    int(row['vegetable'])
+
+    return [[pickUpChickenNewlandsOrders, pickUpChickenChurtonParkOrders, pickUpChickenLowerHuttOrders],
+            [pickUpEggNewlandsOrders, pickUpEggChurtonParkOrders,
+                pickUpEggLowerHuttOrders],
+            [pickUpVegeNewlandsOrders, pickUpVegeChurtonParkOrders, pickUpVegeLowerHuttOrders]]
+
+
+# def getDeliverySummary(csvData):
+#     chickenNewlandsOrders = 0
+#     chickenChurtonParkOrders = 0
+#     chickenLowerHuttOrders = 0
+#     chickenCityOrders = 0
+#     eggNewlandsOrders = 0
+#     eggChurtonParkOrders = 0
+#     eggLowerHuttOrders = 0
+#     eggCityOrders = 0
+#     vegeNewlandsOrders = 0
+#     vegeChurtonParkOrders = 0
+#     vegeLowerHuttOrders = 0
+#     vegeCityOrders = 0
+
+#     # Loop through the Rows
+#     for i, row in csvData.iterrows():
+#         if i > 0:
+#             if int(row['chicken']) > 0 and row['distribution_center'] == 'Newlands':
+#                 chickenNewlandsOrders = chickenNewlandsOrders + \
+#                     int(row['chicken'])
+#     for i, row in csvData.iterrows():
+#         if i > 0:
+#             if int(row['chicken']) > 0 and row['distribution_center'] == 'Churton Park':
+#                 chickenChurtonParkOrders = chickenChurtonParkOrders + \
+#                     int(row['chicken'])
+#     for i, row in csvData.iterrows():
+#         if i > 0:
+#             if int(row['chicken']) > 0 and row['distribution_center'] == 'Lower Hutt':
+#                 pickUpChickenLowerHuttOrders = pickUpChickenLowerHuttOrders + \
+#                     int(row['chicken'])
+#     for i, row in csvData.iterrows():
+#         if i > 0:
+#             if int(row['chicken']) > 0 and row['distribution_center'] == 'City':
+#                 pickUpChickenLowerHuttOrders = pickUpChickenLowerHuttOrders + \
+#                     int(row['chicken'])
+
+#     for i, row in csvData.iterrows():
+#         if i > 0:
+#             if int(row['egg']) > 0 and row['distribution_center'] == 'Newlands':
+#                 pickUpEggNewlandsOrders = pickUpEggNewlandsOrders + \
+#                     int(row['egg'])
+#     for i, row in csvData.iterrows():
+#         if i > 0:
+#             if int(row['egg']) > 0 and row['distribution_center'] == 'Churton Park':
+#                 pickUpEggChurtonParkOrders = pickUpEggChurtonParkOrders + \
+#                     int(row['egg'])
+#     for i, row in csvData.iterrows():
+#         if i > 0:
+#             if int(row['egg']) > 0 and row['distribution_center'] == 'Lower Hutt':
+#                 pickUpEggLowerHuttOrders = pickUpEggLowerHuttOrders + \
+#                     int(row['egg'])
+#     for i, row in csvData.iterrows():
+#         if i > 0:
+#             if int(row['egg']) > 0 and row['distribution_center'] == 'City':
+#                 pickUpEggLowerHuttOrders = pickUpEggLowerHuttOrders + \
+#                     int(row['egg'])
+
+#     for i, row in csvData.iterrows():
+#         if i > 0:
+#             if int(row['egg']) > 0 and row['distribution_center'] == 'Newlands':
+#                 pickUpVegeNewlandsOrders = pickUpVegeNewlandsOrders + \
+#                     int(row['vegetable'])
+#     for i, row in csvData.iterrows():
+#         if i > 0:
+#             if int(row['egg']) > 0 and row['distribution_center'] == 'Churton Park':
+#                 pickUpVegeChurtonParkOrders = pickUpVegeChurtonParkOrders + \
+#                     int(row['vegetable'])
+#     for i, row in csvData.iterrows():
+#         if i > 0:
+#             if int(row['egg']) > 0 and row['distribution_center'] == 'Lower Hutt':
+#                 pickUpVegeLowerHuttOrders = pickUpVegeLowerHuttOrders + \
+#                     int(row['vegetable'])
+#     for i, row in csvData.iterrows():
+#         if i > 0:
+#             if int(row['egg']) > 0 and row['distribution_center'] == 'City':
+#                 pickUpVegeLowerHuttOrders = pickUpVegeLowerHuttOrders + \
+#                     int(row['vegetable'])
+
+#     return [[pickUpChickenNewlandsOrders, pickUpChickenChurtonParkOrders, pickUpChickenLowerHuttOrders],
+#             [pickUpEggNewlandsOrders, pickUpEggChurtonParkOrders,
+#                 pickUpEggLowerHuttOrders],
+#             [pickUpVegeNewlandsOrders, pickUpVegeChurtonParkOrders, pickUpVegeLowerHuttOrders]]
+
 
 if __name__ == "__main__":
     app.run(port=5000)
